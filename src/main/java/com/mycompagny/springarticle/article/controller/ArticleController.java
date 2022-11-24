a7adb9f153d03004b9c9abfa96c624c0a56f4740
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2022-11-24 14:37:02 +0000

    Methode pour les operations d'article 'Service'

---
+++ src/main/java/com/mycompagny/springarticle/article/controller/ArticleController.java
@@ -0,0 +1,10 @@
+package com.mycompagny.springarticle.article.controller;
+
+import com.mycompagny.springarticle.article.service.ArticleService;
+
+public class ArticleController {
+
+    private ArticleService articleService;
+
+
+}
--- src/main/java/com/mycompagny/springarticle/article/service/ArticleService.java
+++ src/main/java/com/mycompagny/springarticle/article/service/ArticleService.java
@@ -2,12 +2,13 @@ package com.mycompagny.springarticle.article.service;
 
 import com.mycompagny.springarticle.article.entity.Article;
 import com.mycompagny.springarticle.article.repository.ArticleRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ArticleService {
-
-
 
     private ArticleRepository articleRepository;
 
@@ -27,5 +28,31 @@ public class ArticleService {
 
 //    methode de modification d'un article
 
+    public ResponseEntity<Article> updateArticle(Article article, Long id){
+
+        Article existingArticle = articleRepository.findById(id).orElse(null);
+        existingArticle.setCode(article.getCode());
+        existingArticle.setLibelle(article.getLibelle());
+        existingArticle.setQte(article.getQte());
+        existingArticle.setDescription(article.getDescription());
+
+        this.articleRepository.save(existingArticle);
+        return  ResponseEntity.ok().body(existingArticle);
+
+
+    }
+
+    // Methode pour supprimer Articles
+
+public String deleteArticle(long id){
+        this.articleRepository.deleteById(id);
+        return "Article supprimer avec succes ";
+}
+
+// Methode pour recuperer une Seul article
+
+    public Article findAtticleArticle(long id){
+        return this.articleRepository.findAtticleArticle(id);
+    }
 
 }
